8a990774eb1a509251185436bc2cfe462e09a2fe
Eric Coe <coeeri001@warren.k12.in.us>
2024-09-20 13:32:51 -0400

    Initial Commit

--- src/App.java
+++ src/App.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class App {
+import java.util.Random;
+
+public class App {
+    public static void main(String[] args) {
+        rollSixDice();
+        rollSixDice();
+        rollSixDice();
+        rollSixDice();
+        rollSixDice();
+        rollSixDice();
+    }
+
+    public static void rollSixDice(){
+            Random rand = new Random();
+            System.out.println("6 dices roll: ");
+            for(int i = 0; i < 6; i++){
+                int roll = rand.nextInt(6) + 1;
+                System.out.print("Roll #" + (i+ 1 ) +  ": " + roll + " ");
+
+    }
+    System.out.println();
+    }
 }
